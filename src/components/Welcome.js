00afbe7fe7f743394e800bd7f1bab1ff3c908070
= <=>
2025-12-12 14:26:21 +0530

    Test1

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,10 @@
-// write code for Welcome component here
 import React from "react";
 
 function Welcome({ name }) {
   return (
     <div>
       <h1>Hey {name}!</h1>
-      <h2>Welcome to School.</h2>
+      <h1>Welcome to School.</h1>
       <p>Hey {name}! Welcome to School.</p>
     </div>
   );
