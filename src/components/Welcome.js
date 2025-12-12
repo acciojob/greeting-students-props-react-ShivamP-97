3159437a5c4d68ee365d8419df00d27ac68e3a41
= <=>
2025-12-12 14:38:16 +0530

    Test4

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,8 @@ function Welcome({ name }) {
   return (
     <div>
       <h1>Hey {name}!</h1>
-      <h1>Welcome to School.</h1>
-      <p>Hey {name}! </p>
-      <p>Welcome to School.</p>
+      <h2>Welcome to School.</h2>
+      <p>Hey {name}! Welcome to School.</p>
     </div>
   );
 }
