f373d49963bbc89d8c3b4d8d96ef41f8253bb9ef
= <=>
2025-12-12 14:23:36 +0530

    Test

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from "react";
+
+function Welcome({ name }) {
+  return (
+    <div>
+      <h1>Hey {name}!</h1>
+      <h2>Welcome to School.</h2>
+      <p>Hey {name}! Welcome to School.</p>
+    </div>
+  );
+}
+
+export default Welcome;
