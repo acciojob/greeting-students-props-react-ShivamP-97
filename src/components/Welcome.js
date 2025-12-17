9cc54af7fce249f4a2e7c2d931e4fef4f5be4a87
= <=>
2025-12-17 21:45:51 +0530

    Test

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-function Welcome({ name }) {
+export default function Welcome({ name }) {
   return (
     <div>
       <h1>Hey {name}!</h1>
       <h2>Welcome to School.</h2>
-      <p>Hey {name}! Welcome to School.</p>
+      <p>This is your welcome page.</p>
     </div>
   );
 }
-
-export default Welcome;
